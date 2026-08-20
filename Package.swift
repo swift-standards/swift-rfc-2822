c5a46b468c3a3b690648be9026d7889709a2cfcf
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:12:05 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -13,31 +13,49 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-rfc-2822",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(name: "RFC 2822", targets: ["RFC 2822"]),
-        .library(name: "RFC 2822 Foundation", targets: ["RFC 2822 Foundation"])
+        .library(name: "RFC 2822 Foundation", targets: ["RFC 2822 Foundation"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-binary-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-binary-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-incits/swift-incits-4-1986.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-parser-primitives.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-parser-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
             name: "RFC 2822",
             dependencies: [
                 .product(name: "Binary Primitives", package: "swift-binary-primitives"),
-                .product(name: "ASCII Serializer Primitives", package: "swift-ascii-serializer-primitives"),
-                .product(name: "Parseable ASCII Primitives", package: "swift-ascii-parser-primitives"),
+                .product(
+                    name: "ASCII Serializer Primitives",
+                    package: "swift-ascii-serializer-primitives"
+                ),
+                .product(
+                    name: "Parseable ASCII Primitives",
+                    package: "swift-ascii-parser-primitives"
+                ),
                 .product(name: "INCITS 4 1986", package: "swift-incits-4-1986"),
-                .product(name: "Parser Primitives", package: "swift-parser-primitives")
+                .product(name: "Parser Primitives", package: "swift-parser-primitives"),
             ]
         ),
         .target(
@@ -49,13 +67,13 @@ let package = Package(
         .testTarget(
             name: "RFC 2822 Foundation Tests",
             dependencies: [
-                "RFC 2822",
+                "RFC 2822"
             ]
         ),
         .testTarget(
             name: "RFC 2822 Tests",
             dependencies: [
-                "RFC 2822",
+                "RFC 2822"
             ]
         ),
     ],
